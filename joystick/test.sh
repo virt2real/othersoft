789ec6bde85f28ce0567318c56cd8bb1c46f1e13
Gol <root@virt2real.(none)>
2014-02-01 15:07:43 -0500

    fixed

--- joystick/test.sh
+++ joystick/test.sh
@@ -1,5 +1,4 @@
-./joyevents -d  /dev/input/by-id/usb-Logitech_Logitech_Cordless_RumblePad_2-event-joystick -h localhost -p 2222 -v
-#./joyevents -d  /dev/input/event13 -h localhost -p 2222 -v
+./joyevents -d  /dev/input/event0 -v
 
 
 
